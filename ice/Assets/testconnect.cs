413269ddc7f0e38a7aabcf9f8e9950cde45e08e5
lmxy0212 <lmxyngnl@gmail.com>
2020-11-24 23:16:23 -0500

    import and tested Photon

---
+++ ice/Assets/testconnect.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class testconnect : MonoBehaviourPunCallbacks
+{
+    //// Start is called before the first frame update
+    void Start()
+    {
+        print("Connecting to server....");
+        PhotonNetwork.GameVersion = "0.0.1";
+        PhotonNetwork.ConnectUsingSettings();
+
+    }
+    public override void OnConnectedToMaster()
+    {
+        print("Connected to server!");
+    }
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        //base.OnDisconnected(cause);
+        print("Disconnected from server" + cause.ToString());
+    }
+}

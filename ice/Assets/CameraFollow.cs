0a220c3f2762eb1d4cdc95d6ece1ec5cf539d620
sofía sánchez-zárate <47282146+sofiasanchez985@users.noreply.github.com>
2020-12-18 17:15:54 -0500

    camera working on start!
    CameraFollow script added to a view point sphere to move MRTK's main camera to above the ice sheet rather than the origin

---
+++ ice/Assets/CameraFollow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Camera.main.transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+        Camera.main.GetComponent<Camera>().farClipPlane = 15000f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

f80487348c5965774d8cded349edb6f116b6107b
lmxy0212 <lmxyngnl@gmail.com>
2020-12-20 10:05:28 -0500

    backup...but still notworking..

--- ice/Assets/Scripts/transferownership.cs
+++ ice/Assets/Scripts/transferownership.cs
@@ -8,7 +8,7 @@ public class transferownership : MonoBehaviourPunCallbacks
 {
 
 
-    private GameObject pickup;
+    //private GameObject pickup;
 
     // Start is called before the first frame update
     //void Start()
@@ -19,31 +19,20 @@ public class transferownership : MonoBehaviourPunCallbacks
     ////// Update is called once per frame
     void Update()
     {
-        if (pickup.GetComponent<PhotonView>().IsMine)
-        {
-            //grabObject(); 
-        }
-        else
-        {
-            base.photonView.RequestOwnership();
-            //pickup.GetComponent<PhotonView>().TransferOwnership(PhotonNetwork.player.ID);
-            //grabObject();
-        }
+        //if (pickup.GetComponent<PhotonView>().IsMine)
+        //{
+        //    //grabObject(); 
+        //}
+        //else
+        //{
+        //    base.photonView.RequestOwnership();
+        //    //pickup.GetComponent<PhotonView>().TransferOwnership(PhotonNetwork.player.ID);
+        //    //grabObject();
+        //}
     }
 
-    //void InteractionManager_InteractionSourcePressed(InteractionSourcePressedEventArgs args)
-    //{
-    //    var interactionSourceState = args.state;
-    //    var headPose = interactionSourceState.headPose;
-    //    RaycastHit raycastHit;
-    //    if (Physics.Raycast(headPose.position, headPose.forward, out raycastHit, 10))
-    //    {
-    //        var targetObject = raycastHit.collider.gameObject;
-    //        base.photonView.RequestOwnership();
-    //    }
-    //}
-    //private void OnTriggerExit(Collider collider)
-    //{
-    //    pickup = null;
-    //}
+    private void OnTriggerEnter(Collider collider)
+    {
+        base.photonView.RequestOwnership();
+    }
 }

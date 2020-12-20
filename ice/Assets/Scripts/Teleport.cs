e141780bcfbb5edb0699663c5bf8b5a1da0938e5
sofía sánchez-zárate <47282146+sofiasanchez985@users.noreply.github.com>
2020-12-19 20:10:45 -0500

    teleport rotations + scaling
    added correct camera rotations to teleport and scaled up the universe too

--- ice/Assets/Scripts/Teleport.cs
+++ ice/Assets/Scripts/Teleport.cs
@@ -9,40 +9,53 @@ public class Teleport : MonoBehaviour
     public GameObject Target2014;
     public GameObject Target2015;
     public Camera cam;
-
-    //add rotations to camera
-    //fix positions each thing goes to
-    //scale up the universe!!
+    public GameObject Universe;
 
     // Start is called before the first frame update
     void Start()
     {
-        cam.transform.position = StartTarget.transform.position;
+        StartPos();
+        //cam.transform.position = StartTarget.transform.position;
     }
 
+    /*
     void ResetCamera()
     {
         cam.transform.position = Vector3.zero;
+    }
+    */
+    
+    void ScaleUniverse()
+    {
+        Vector3 scaleChange = new Vector3(.1f, .1f, .1f);
+        Universe.transform.localScale = scaleChange;
     }
 
     public void StartPos()
     {
         cam.transform.position = StartTarget.transform.position;
+        //cam.transform.rotation = StartTarget.transform.rotation;
     }
 
 
     public void Move2011()
     {
-        cam.transform.position = Target2011.transform.position;
+        ScaleUniverse();
+        cam.transform.rotation = Target2011.transform.rotation;
+        cam.transform.position = Target2011.transform.position;        
     }
 
     public void Move2014()
     {
+        ScaleUniverse();
+        cam.transform.rotation = Target2014.transform.rotation;
         cam.transform.position = Target2014.transform.position;
     }
 
     public void Move2015()
     {
+        ScaleUniverse();
+        cam.transform.rotation = Target2015.transform.rotation;
         cam.transform.position = Target2015.transform.position;
     }
 

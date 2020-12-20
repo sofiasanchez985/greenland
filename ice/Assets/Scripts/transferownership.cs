c424dc485f4cad1210183e42ab444ccf1ba817ab
lmxy0212 <lmxyngnl@gmail.com>
2020-12-20 09:02:45 -0500

    test

---
+++ ice/Assets/Scripts/transferownership.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class transferownership : MonoBehaviourPunCallbacks
+{
+
+
+    //private GameObject pickup;
+
+    // Start is called before the first frame update
+    //void Start()
+    //{
+
+    //}
+
+    ////// Update is called once per frame
+    //void Update()
+    //{
+    //    if (pickup.GetComponent<PhotonView>().IsMine)
+    //    {
+    //        //grabObject(); 
+    //    }
+    //    else
+    //    {
+    //        //pickup.GetComponent<PhotonView>().TransferOwnership(PhotonNetwork.player.ID);
+    //        //grabObject();
+    //    }
+    //}
+    private void OnTriggerEnter(Collider collider)
+    {
+        //pickup = collider.gameObject;
+        base.photonView.RequestOwnership();
+    }
+
+    //private void OnTriggerExit(Collider collider)
+    //{
+    //    pickup = null;
+    //}
+}

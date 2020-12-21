4e7c06a5c0a6a37feb224f0bf4f9fb74a2002fa8
lmxy0212 <lmxyngnl@gmail.com>
2020-12-21 02:48:52 -0500

    yayyyy fix minor things

--- ice/Assets/Scripts/PhotonLobby.cs
+++ ice/Assets/Scripts/PhotonLobby.cs
@@ -17,17 +17,17 @@ public class PhotonLobby : MonoBehaviourPunCallbacks
     // Start is called before the first frame update
     void Start()
     {
-        print("Connecting to server....");
+        Debug.Log("Connecting to server....");
         PhotonNetwork.ConnectUsingSettings(); //connect to master photon server
     }
     public override void OnConnectedToMaster()
     {
-        print("Connected to server!");
+        Debug.Log("Connected to server!");
         startButton.SetActive(true);
     }
     public override void OnDisconnected(DisconnectCause cause)
     {
-        print("Disconnected from server" + cause.ToString());
+        Debug.Log("Disconnected from server" + cause.ToString());
     }
 
     public void OnStartButtonClick()
@@ -38,7 +38,7 @@ public class PhotonLobby : MonoBehaviourPunCallbacks
     }
     public override void OnJoinRandomFailed(short returnCode, string message)
     {
-        print("Faied to join random room, no room available :(");
+        Debug.Log("Faied to join random room, no room available :(");
         CreateRoom();
     }
     void CreateRoom()
@@ -46,23 +46,23 @@ public class PhotonLobby : MonoBehaviourPunCallbacks
         int randomRoomName = Random.Range(0, 10);
         RoomOptions roomOps = new RoomOptions() { IsVisible = true, IsOpen = true, MaxPlayers = 2 };
         PhotonNetwork.CreateRoom("Room" + randomRoomName, roomOps);
-        print("Created a new room.");
+        Debug.Log("Created a new room.");
     }
     public override void OnCreateRoomFailed(short returnCode, string message)
     {
-        print("Faied to create room :(");
+        Debug.Log("Faied to create room :(");
         CreateRoom();
     }
     public override void OnJoinedRoom()
     {
-        print("You are now in a room!!!");
+        Debug.Log("You are now in a room!!!");
     }
     public void OnCancellButtonClick()
     {
         startButton.SetActive(true);
         cancellButton.SetActive(false);
         PhotonNetwork.LeaveRoom();
-        print("You leaft teh room.");
+        Debug.Log("You leaft teh room.");
     }
 
 

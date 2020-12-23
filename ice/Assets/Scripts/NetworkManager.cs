05181dd7f96cb44cebf2dacfaa24c50e0d443d27
sofía sánchez-zárate <47282146+sofiasanchez985@users.noreply.github.com>
2020-12-23 06:16:57 -0500

    pun x oculus avatars

---
+++ ice/Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections.Generic;
+using ExitGames.Client.Photon;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class NetworkManager : MonoBehaviour, IConnectionCallbacks, IMatchmakingCallbacks, IOnEventCallback
+{
+    public const byte InstantiateVrAvatarEventCode = 1; // example code, change to any value between 1 and 199
+
+    [SerializeField]
+    private string roomName = "oculusPhotonRoom"; // example dummy room name, you can skip using a hardcoded name altogether
+
+    private void OnEnable()
+    {
+        PhotonNetwork.AddCallbackTarget(this);
+    }
+
+    private void OnDisable()
+    {
+        PhotonNetwork.RemoveCallbackTarget(this);
+    }
+
+    private void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    #region IConnectionCallbacks
+
+
+    void IConnectionCallbacks.OnConnected()
+    {
+    }
+
+    void IConnectionCallbacks.OnConnectedToMaster()
+    {
+        // this is an example of how to join a room quickly
+        // use any of the other available matchmaking ways to join rooms
+        PhotonNetwork.JoinOrCreateRoom(roomName, new RoomOptions(), TypedLobby.Default);
+    }
+
+    void IConnectionCallbacks.OnDisconnected(DisconnectCause cause)
+    {
+    }
+
+    void IConnectionCallbacks.OnRegionListReceived(RegionHandler regionHandler)
+    {
+    }
+
+    void IConnectionCallbacks.OnCustomAuthenticationResponse(Dictionary<string, object> data)
+    {
+    }
+
+    void IConnectionCallbacks.OnCustomAuthenticationFailed(string debugMessage)
+    {
+    }
+
+    #endregion
+
+    #region IMatchmakingCallbacks
+
+    void IMatchmakingCallbacks.OnJoinedRoom()
+    {
+        GameObject localAvatar = Instantiate(Resources.Load("LocalAvatar")) as GameObject;
+        PhotonView photonView = localAvatar.GetComponent<PhotonView>();
+
+        if (PhotonNetwork.AllocateViewID(photonView))
+        {
+            RaiseEventOptions raiseEventOptions = new RaiseEventOptions
+            {
+                CachingOption = EventCaching.AddToRoomCache,
+                Receivers = ReceiverGroup.Others
+            };
+
+            PhotonNetwork.RaiseEvent(InstantiateVrAvatarEventCode, photonView.ViewID, raiseEventOptions, SendOptions.SendReliable);
+        }
+        else
+        {
+            Debug.LogError("Failed to allocate a ViewId.");
+
+            Destroy(localAvatar);
+        }
+    }
+
+    void IMatchmakingCallbacks.OnFriendListUpdate(List<FriendInfo> friendList)
+    {
+    }
+
+    void IMatchmakingCallbacks.OnCreatedRoom()
+    {
+    }
+
+    void IMatchmakingCallbacks.OnCreateRoomFailed(short returnCode, string message)
+    {
+    }
+
+    void IMatchmakingCallbacks.OnJoinRoomFailed(short returnCode, string message)
+    {
+    }
+
+    void IMatchmakingCallbacks.OnJoinRandomFailed(short returnCode, string message)
+    {
+    }
+
+    void IMatchmakingCallbacks.OnLeftRoom()
+    {
+    }
+
+    #endregion
+
+    void IOnEventCallback.OnEvent(EventData photonEvent)
+    {
+        if (photonEvent.Code == InstantiateVrAvatarEventCode)
+        {
+            GameObject remoteAvatar = Instantiate(Resources.Load("RemoteAvatar")) as GameObject;
+            PhotonView photonView = remoteAvatar.GetComponent<PhotonView>();
+            photonView.ViewID = (int)photonEvent.CustomData;
+        }
+    }
+
+}
---
+++ ice/Assets/Scripts/PhotonAvatarView.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using System.IO;
+
+public class PhotonAvatarView : MonoBehaviourPunCallbacks, IPunObservable
+{
+    private PhotonView photonView;
+    private OvrAvatar ovrAvatar;
+    private OvrAvatarRemoteDriver remoteDriver;
+
+    private List<byte[]> packetData;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        photonView = GetComponent<PhotonView>();
+
+        if (photonView.IsMine)
+        {
+            ovrAvatar = GetComponent<OvrAvatar>();
+            ovrAvatar.RecordPackets = true;
+            ovrAvatar.PacketRecorded += OnLocalAvatarPacketRecorded;
+
+            packetData = new List<byte[]>();
+        }
+        else
+        {
+            remoteDriver = GetComponent<OvrAvatarRemoteDriver>();
+        }
+    }
+
+    public void OnDisable()
+    {
+        if (photonView.IsMine)
+        {
+            ovrAvatar.RecordPackets = false;
+            ovrAvatar.PacketRecorded -= OnLocalAvatarPacketRecorded;
+        }
+    }
+
+    private int localSequence;
+
+    public void OnLocalAvatarPacketRecorded(object sender, OvrAvatar.PacketEventArgs args)
+    {
+        if (!PhotonNetwork.InRoom || (PhotonNetwork.CurrentRoom.PlayerCount < 2))
+        {
+            return;
+        }
+
+        using (MemoryStream outputStream = new MemoryStream())
+        {
+            BinaryWriter writer = new BinaryWriter(outputStream);
+
+            var size = Oculus.Avatar.CAPI.ovrAvatarPacket_GetSize(args.Packet.ovrNativePacket);
+            byte[] data = new byte[size];
+            Oculus.Avatar.CAPI.ovrAvatarPacket_Write(args.Packet.ovrNativePacket, size, data);
+
+            writer.Write(localSequence++);
+            writer.Write(size);
+            writer.Write(data);
+
+            packetData.Add(outputStream.ToArray());
+        }
+    }
+
+    private void DeserializeAndQueuePacketData(byte[] data)
+    {
+        using (MemoryStream inputStream = new MemoryStream(data))
+        {
+            BinaryReader reader = new BinaryReader(inputStream);
+            int remoteSequence = reader.ReadInt32();
+
+            int size = reader.ReadInt32();
+            byte[] sdkData = reader.ReadBytes(size);
+
+            System.IntPtr packet = Oculus.Avatar.CAPI.ovrAvatarPacket_Read((System.UInt32)data.Length, sdkData);
+            remoteDriver.QueuePacket(remoteSequence, new OvrAvatarPacket { ovrNativePacket = packet });
+        }
+    }
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.IsWriting)
+        {
+            if (packetData.Count == 0)
+            {
+                return;
+            }
+
+            stream.SendNext(packetData.Count);
+
+            foreach (byte[] b in packetData)
+            {
+                stream.SendNext(b);
+            }
+
+            packetData.Clear();
+        }
+
+        if (stream.IsReading)
+        {
+            int num = (int)stream.ReceiveNext();
+
+            for (int counter = 0; counter < num; ++counter)
+            {
+                byte[] data = (byte[])stream.ReceiveNext();
+
+                DeserializeAndQueuePacketData(data);
+            }
+        }
+    }
+
+        // Update is called once per frame
+        void Update()
+    {
+        
+    }
+}

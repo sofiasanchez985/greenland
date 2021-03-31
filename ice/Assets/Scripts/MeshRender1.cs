ff47c16b25e1749753f0ddebb9dbdb607ba79154
sofía sánchez-zárate <sofia.sanchez@columbia.edu>
2021-03-31 16:04:18 -0400

    uv fixed
    uv fixed, but alignment issues still :/

--- ice/Assets/Scripts/MeshRender1.cs
+++ ice/Assets/Scripts/MeshRender1.cs
@@ -6,7 +6,6 @@ using UnityEngine;
 public class MeshRender1 : MonoBehaviour
 {
 
-    // eliminate ySize, it's one bc one triangle only yeet
     // make the yStart or yEnd positions translate onto where the mesh starts automatically
     // correct values on 2011 and 2011-2015 flipped
     // basically went around the issue and made a big triangle rather than +6,000 small layers(:
@@ -43,8 +42,8 @@ public class MeshRender1 : MonoBehaviour
     private void Awake()
     {
         ProcessData();
-        //Generate();
-        StartCoroutine(Generate());
+        Generate();
+        //StartCoroutine(Generate());
     }
 
     private void ProcessData()
@@ -53,14 +52,15 @@ public class MeshRender1 : MonoBehaviour
         // Process CSV Data
         pointList = CSVReader.Read(inputfile);
 
-        //float radarHeightF = (yEnd - yStart) * scaleFactor;
-        //radarHeight = (int)radarHeightF;
         radarHeight = Math.Abs(yEnd - yStart) * scaleFactor;
 
         // Declare list of strings, fill with keys (column names)
         // The index of pointList doesn't matter, keys are same for every dictionary
         List<string> columnList = new List<string>(pointList[0].Keys);
 
+        /*
+        Debugging things
+        
         // Print number of keys (using .count)
         Debug.Log("There are " + columnList.Count + " columns in CSV");
 
@@ -68,6 +68,7 @@ public class MeshRender1 : MonoBehaviour
             Debug.Log("Column name is " + key);
 
         Debug.Log("point list count: " + pointList.Count);
+        */
 
         // Assign column name from columnList to Name variables
         xName = columnList[columnX];
@@ -75,28 +76,29 @@ public class MeshRender1 : MonoBehaviour
 
     }
 
-    //do stuff with camera
-
-    private IEnumerator Generate()
-    //private void Generate()
+    //private IEnumerator Generate()
+    private void Generate()
     {
-        WaitForSeconds wait = new WaitForSeconds(0.005f);
+        //WaitForSeconds wait = new WaitForSeconds(0.005f);
 
-        Debug.Log("GOING");
+        //Debug.Log("GOING");
 
         GetComponent<MeshFilter>().mesh = mesh = new Mesh();
         mesh.name = "Procedural Grid";
 
-        Debug.Log("first y/z value: " + pointList[0][zName]);
+        //Debug.Log("first y/z value: " + pointList[0][zName]);
 
         // Added Math.Abs(yStart) to size in order to fit negative y start values
         // declaring a list of Vector3's with the size of the horizonal point list * height of the mesh
-        //vertices = new Vector3[(pointList.Count + 1) * (ySize + Math.Abs(yStart) + 1)];
+        // vertices = new Vector3[(pointList.Count + 1) * (ySize + Math.Abs(yStart) + 1)];
         // one bc one triangle 11!!1!!!!!!!!
         vertices = new Vector3[(pointList.Count + 1) * (1 + 1)];
         Vector2[] uv = new Vector2[vertices.Length];
 
-        //for (int y = yStart, w = 0; y <= ySize; y++)
+        float[] distances = new float[pointList.Count];
+        distances[0] = 0;
+
+        // ySize public var, instantiates to 1, so loop goes twice for top and bottom row
         for (int y = 0, w = 0; y <= ySize; y++)
         {
             //changed from <= to <
@@ -106,22 +108,38 @@ public class MeshRender1 : MonoBehaviour
                 float zPos = (float)pointList[i][zName] * scaleFactor;
                 vertices[w] = new Vector3(xPos, y * radarHeight, zPos);
 
-                // Spheres for testing/debugging
+                /*
+                Spheres for testing/debugging
 
-                //GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-                //sphere.transform.position = new Vector3((float)xPos, y * 100, (float)zPos);
-                //sphere.transform.position = new Vector2((float)xPos / pointList.Count, (float)y / ySize);
+                GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+                sphere.transform.position = new Vector3((float)xPos, y * 100, (float)zPos);
+                sphere.transform.position = new Vector2((float)xPos / pointList.Count, (float)y / ySize);
+                */
 
-                // Mapping coordinates to fit between 0,1
+                // Calculate Along Track Distances
+                if (y == 0 && w > 1)
+                {
+                    double deltaX = Math.Pow(vertices[w - 1].x - vertices[w].x, 2);
+                    double deltaZ = Math.Pow(vertices[w - 1].z - vertices[w].z, 2);
+                    double dist = Math.Sqrt(deltaX + deltaZ);
 
-                float xStart = (float)pointList[0][xName];
-                float xEnd = (float)pointList[pointList.Count - 1][xName];
-                float xCurrent = (float)pointList[i][xName];
-                float xUV = (xCurrent - xStart) / (xEnd - xStart);
-
-                uv[w] = new Vector2(xUV, (float)y / ySize);
+                    distances[w] = (float)dist + distances[w-1];
+                }
+                
             }
         }
+
+        // Mapping UV coordinates
+        for (int y = 0, w = 0; y <= ySize; y++)
+        {
+            for (int i = 0; i < pointList.Count; i++, w++)
+            {
+                float TotalDistance = distances[pointList.Count - 1];
+                float DistUV = distances[i] / TotalDistance;
+                uv[w] = new Vector2(DistUV, (float)y / ySize);
+            }
+        }
+
         mesh.vertices = vertices;
         mesh.uv = uv;
 
@@ -138,7 +156,7 @@ public class MeshRender1 : MonoBehaviour
                 //subtracted one from pointList.Count
                 triangles[ti + 5] = vi + pointList.Count + 1;
                 mesh.triangles = triangles;
-                yield return wait;
+                //yield return wait;
             }
         }
         //mesh.triangles = triangles;
@@ -153,16 +171,5 @@ public class MeshRender1 : MonoBehaviour
         Debug.Log("position: " + mytransform.localPosition);
         
     }
-
-    /*
-    private void FixedUpdate()
-    {
-        //float yPosScaled = yStart * scaleFactor;
-        //Debug.Log("YPOS " + yPosScaled);
-        //meshObject.transform.position = new Vector3(0, 0, 0);
-        //meshObject.transform.position = new Vector3(0, yPosScaled, 0);
-        Debug.Log("transform print " + mytransform.position);
-    }
-    */
 
 }

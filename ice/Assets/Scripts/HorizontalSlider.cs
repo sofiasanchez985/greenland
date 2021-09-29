092bd58bfb62863920b7855eeac9095af703aebc
sofía sánchez-zárate <sofia.sanchez@columbia.edu>
2021-09-29 11:27:48 -0400

    Sliders added to Antarctica scene for distortion

---
+++ ice/Assets/Scripts/HorizontalSlider.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Microsoft.MixedReality.Toolkit.UI;
+
+public class HorizontalSlider : MonoBehaviour
+{
+
+    public Transform TargetObject;
+
+    public void OnSliderUpdated(SliderEventData eventData)
+    {
+        // Rotate the target object using Slider's eventData.NewValue
+        TargetObject.localScale = new Vector3(25 + (10 * eventData.NewValue), TargetObject.localScale.y, TargetObject.localScale.z);
+        Debug.Log(string.Format("Horizontal: {0}", 25 + (10 * eventData.NewValue)));
+    }
+
+}
---
+++ ice/Assets/Scripts/VerticalSlider.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Microsoft.MixedReality.Toolkit.UI;
+
+public class VerticalSlider : MonoBehaviour
+{
+
+    public Transform TargetObject;
+
+    public void OnSliderUpdated(SliderEventData eventData)
+    {
+        // Rotate the target object using Slider's eventData.NewValue
+        TargetObject.localScale = new Vector3(TargetObject.localScale.x, 5 + (10 * eventData.NewValue), TargetObject.localScale.z);
+        Debug.Log(string.Format("Vertical: {0}", 5 + (10 * eventData.NewValue)));
+    }
+
+}

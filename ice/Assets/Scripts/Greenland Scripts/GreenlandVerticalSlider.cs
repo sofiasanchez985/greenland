43773707d5c7825c6b4966f24477122af9f8ef06
sofía sánchez-zárate <sofia.sanchez@columbia.edu>
2021-10-17 19:34:41 -0400

    Reorganized Greenland & Antarctica Scripts; Fixed vertical scaling in Greenland

---
+++ ice/Assets/Scripts/Greenland Scripts/GreenlandVerticalSlider.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Microsoft.MixedReality.Toolkit.UI;
+
+public class GreenlandVerticalSlider : MonoBehaviour
+{
+
+    public Transform IceSheet;
+
+    public void OnSliderUpdated(SliderEventData eventData)
+    {
+        // Rotate the target object using Slider's eventData.NewValue
+        IceSheet.localScale = new Vector3(IceSheet.localScale.x, 4 * eventData.NewValue, IceSheet.localScale.z);
+    }
+
+}

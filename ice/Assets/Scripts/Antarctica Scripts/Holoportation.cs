a50ff1ff82df4e3007ad436206c932468613441d
sofía sánchez-zárate <sofia.sanchez@columbia.edu>
2021-10-27 19:42:06 -0400

    Scaling menu added to L840 & 'holoportation' script added to move objects on play

---
+++ ice/Assets/Scripts/Antarctica Scripts/Holoportation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Holoportation : MonoBehaviour
+{
+
+    public GameObject Antarctica;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Antarctica.transform.position = new Vector3(-10.7f, 11f, 151.2f);
+    }
+
+    /*
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    */
+}
